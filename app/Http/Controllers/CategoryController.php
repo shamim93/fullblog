a82a84779707583e07c773ff7ae2069d4d31e59e
Shamim Hossen <shamim41.cse@gmail.com>
2020-07-20 00:41:14 +0600

    category updated

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,82 +3,81 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Category;
+use Illuminate\Support\Str;
+use Illuminate\Validation\Rule;
 
 class CategoryController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+   
     public function index()
     {
-        //
+        $categorie = Category::latest()->paginate(20);
+        return view('admin.category.index',['categories' =>$categorie]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+   
     public function create()
     {
-        //
+        return view('admin.category.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+   
     public function store(Request $request)
     {
-        //
+        // $categoryData = $request->validate([
+        //     'name'  => 'required|unique:categories,name',
+        //     // 'slug'  => ,
+        // ]);
+        $this->validate($request,[
+            'name'  => 'required|unique:categories,name',
+            // 'slug'  => ,
+        ]);
+        $category=Category::create([
+            'name'  => $request->name,
+            'description'  => $request->description,
+            'slug'         =>Str::slug($request->name,'-'),
+        ]);
+        
+        return redirect()->route('categories.index')->with('success', 'Category Added');
+        
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+   
     public function edit($id)
     {
-        //
+        $category = Category::findOrFail($id);
+        return view('admin.category.edit',['category'=>$category]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
+    
+    public function update(Request $request, Category $category)
     {
-        //
+        $this->validate($request,[
+            'name'  => "required|unique:categories,name,".$category->id,
+            // 'slug'  => ,
+        ]);
+       
+        $category->name = $request->name;
+        $category->slug = Str::slug($request->name,'-');
+        $category->description = $request->description;
+        $category->save();
+        
+        return redirect()->route('categories.index')->with('success', 'Category Updated');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
+   
+    public function destroy(Category $category)
     {
-        //
+        if($category){
+            $category->delete();
+            return redirect()->route('categories.index')->with('success', 'Category Deleted');
+        }
+        return back();
     }
 }
